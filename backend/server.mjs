9622b98b01175f9336b03710c9eb914d26f3d48b
Hanfei Qu <hanfei.qu@identos.ca>
2024-06-08 20:13:49 -0400

    remove unused static reference

--- backend/server.mjs
+++ backend/server.mjs
@@ -56,8 +56,6 @@ app.post('/send', async (req, res) => {
   }
 });
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
