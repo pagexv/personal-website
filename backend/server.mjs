3252799e5cf662c54e35b4f94569fb02ca153fb4
Hanfei Qu <hanfei.qu@identos.ca>
2024-06-08 20:15:31 -0400

    remove unused static reference

--- backend/server.mjs
+++ backend/server.mjs
@@ -57,11 +57,7 @@ app.post('/send', async (req, res) => {
 });
 
 
-app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
